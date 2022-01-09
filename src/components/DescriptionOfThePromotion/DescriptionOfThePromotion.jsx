5aaf57be67faef93dd9df5da2c787e9a6b3df003
alinaruchkina <alina.kartyschova@gmail.com>
2022-01-10 01:36:48 +0300

    added information about the action

--- src/components/DescriptionOfThePromotion/DescriptionOfThePromotion.jsx
+++ src/components/DescriptionOfThePromotion/DescriptionOfThePromotion.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import styles from './DescriptionOfThePromotion.module.css';
 import icon from '../../utils/images/icon_of_the_ice_cream.png';
+import blue_ice_cream from '../../utils/images/blue_ice_cream.jpg';
+import green_ice_cream from '../../utils/images/green_ice_cream.jpg';
+import rose_ice_cream from '../../utils/images/rose_ice_cream.jpg';
 
 function DescriptionOfThePromotion(params) {
   return (
@@ -9,6 +12,30 @@ function DescriptionOfThePromotion(params) {
         <div className={styles.headline}> Find Your Taste </div>
         <div className={styles.promotion}>Buy any ice cream @ get One Free</div>
         <img className={styles.image_with_ice_cream} src={icon} alt="icon of the ice cream" />
+      </div>
+      <div className={styles.content_of_promotion}>
+        <div className={styles.imageContainerBlue}>
+          <div className={styles.informationAboutIceCream}>
+            <div className={styles.raspberryFlavour}>Raspberry Flavour</div>
+            <div className={styles.icуCreamTaste}>Blue Moon</div>
+          </div>
+        </div>
+        <div>
+          <div className={styles.imageContainerGreen}>
+            <div className={styles.informationAboutIceCream}>
+              <div className={styles.meldOfFlavor}>Meld of Flavor</div>
+              <div className={styles.icуCreamTaste}>Neapolitan</div>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div className={styles.imageContainerRose}>
+            <div className={styles.informationAboutIceCream}>
+              <div className={styles.naturalSweetness}>Natural Sweetness</div>
+              <div className={styles.icуCreamTaste}>Lucuma</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
